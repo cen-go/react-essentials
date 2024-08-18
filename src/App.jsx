dff204d6315cbe3e44739c046b7c3f8b7224d172
luzumsuzadam <cengizulutas123123@gmail.com>
2024-08-18 23:31:23 +0300

    18 August end-day

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import Header from "./components/Header";
 import CoreConcept from "./components/CoreConcept";
 import { CORE_CONCEPTS } from "./data";
+import TabButton from "./components/TabButton";
 
 function App() {
+  function handleSelect(selectedTab) {
+    console.log(`${selectedTab} tab is selected.`);    
+  }
+
   return (
     <div>
       <Header />
@@ -12,6 +17,15 @@ function App() {
           <ul>
             {CORE_CONCEPTS.map( concept => <CoreConcept {...concept} /> )}            
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("Components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
