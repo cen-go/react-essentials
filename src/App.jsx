4ddcc2ecec7b29e614f774fc721fe58accd9db1d
luzumsuzadam <cengizulutas123123@gmail.com>
2024-08-18 19:50:37 +0300

    18 August

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
+import { CORE_CONCEPTS } from "./data";
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!!</h2>
+        <section id="core-concepts">          
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map( concept => <CoreConcept {...concept} /> )}            
+          </ul>
+        </section>
       </main>
     </div>
   );
